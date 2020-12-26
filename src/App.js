3428995c4149fe9bece067876a6a1e938e2b2d67
sergeisaz <sergeisaz53@gmail.com>
2020-12-26 22:23:36 +0300

    26 12 2020 61 lessons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ContentContainer from "./components/Content/ContentContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = (props) => {
 
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
-                <Header/>
+                <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
                    {/* <Route path='/dialogs' component={Dialogs}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
-const Header = () => {
+import {NavLink} from "react-router-dom";
+const Header = (props) => {
     return     <header className={s.header}>
                     <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt=""/>
+
+                    <div className={s.loginBlock}>
+                        { props.isAuth ? props.login
+                        :<NavLink to={'/login'}>Login</NavLink>}
+                    </div>
                 </header>;
 }
 
