679ba5f6e583497943f7d17ab915d26859dccba3
sergeisaz <sergeisaz53@gmail.com>
2020-12-27 20:37:51 +0300

    27 12 2020 71 lessons

--- src/components/Content/ContentContainer.jsx
+++ src/components/Content/ContentContainer.jsx
@@ -35,7 +35,7 @@ let mapStateToProps = (state) => ({
 export default compose(
     connect(mapStateToProps,{getUserProfile}),
     withRouter,
-    withAuthRedirect
+    //withAuthRedirect
 )(ContentContainer);
 
 
--- src/components/Content/ProfileInfo/ProfileInfo.jsx
+++ src/components/Content/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -9,14 +10,14 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div>
+           {/* <div>
                 <img
                     src="https://static9.depositphotos.com/1594308/1110/i/600/depositphotos_11107478-stock-photo-fantasy.jpg"
                     alt=""/>
-            </div>
+            </div>*/}
             <div className={s.descriptionBlock}>
                <img src={props.profile.photos.large}  />
-                ava + description
+                <ProfileStatus status={"Hello my friends"}/>
             </div>
         </div>
     )
