d2eeec0d1995906bfc5fdbf45a2eeb3c40277864
sergeisaz <sergeisaz53@gmail.com>
2020-12-29 17:58:22 +0300

    29 12 2020 lessons 89

--- src/components/Content/MyPosts/MyPosts.jsx
+++ src/components/Content/MyPosts/MyPosts.jsx
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 import {Field, reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../../utils/validators/validators";
 import {Textarea} from "../../common/preloader/FormsControls/FormsControls";
 
-const MyPosts = (props) => {
+class MyPosts extends PureComponent {
 
-    let postsElements = props.posts.map(p =>
-        <Post message={p.message} likesCount={p.likesCount}/>);
+    render() {
+        console.log("Render");
+        let postsElements = this.props.posts.map(p =>
+            <Post message={p.message} likesCount={p.likesCount}/>);
 
-    let newPostElement = React.createRef();
+        let newPostElement = React.createRef();
 
-    let onAddPost = (values) => {
-        props.addPost(values.newPostText);
-    }
+        let onAddPost = (values) => {
+            this.props.addPost(values.newPostText);
+        }
 
 
-    return (
-        <div className={s.postsBlock}>
-            <div>
-                <h3> My posts </h3>
+        return (
+            <div className={s.postsBlock}>
+                <div>
+                    <h3> My posts </h3>
                     <AddNewPostFormRedux onSubmit={onAddPost}/>
-                <div className={s.posts}>
-                    new post
-                    {postsElements}
+                    <div className={s.posts}>
+                        new post
+                        {postsElements}
+                    </div>
                 </div>
-            </div>
 
-        </div>
-    )
+            </div>
+        )
+    }
 }
+
 const maxLength10 = maxLengthCreator(10);
 let AddNewPostForm = (props) =>{
     return (
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,6 +1,7 @@
 import {profileAPI, UsersAPI as usersAPI} from "../api/api";
 
 const ADD_POST = 'ADD_POST';
+const DELETE_POST = 'DELETE_POST';
 
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
@@ -43,6 +44,8 @@ const profileReducer = (state = initialState, action) => {
         case SET_USER_PROFILE: {
             return {...state, profile: action.profile}
         }
+        case DELETE_POST:
+            return  {...state, posts: state.posts.filter(p=>p.id !=action.postId)}
         default:
             return state;
     }
@@ -51,6 +54,7 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText})
 export const setUserProfile = (profile) => ( {type: SET_USER_PROFILE, profile} )
 export const setStatus = (status) => ( {type: SET_STATUS, status} )
+export const deletePost = (postId) => ( {type: DELETE_POST, postId} )
 export const getUserProfile = (userId) => (dispatch) =>{
     usersAPI.getProfile(userId).then(response =>{
         dispatch(setUserProfile(response.data));
